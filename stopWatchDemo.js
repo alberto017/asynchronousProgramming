c751f2aa1d51f8f7646e92b2709ef2760039307b
alberto017 <xavihdz09@gmail.com>
2017-09-22 02:18:19 -0500

    feat: Full exercise

---
+++ stopWatchDemo.js
@@ -0,0 +1,71 @@
+
+// variables globales
+var started = false;
+var time = 0;
+var timesDisplayed = [];
+
+
+init();
+
+// Definimos las funciones
+function display(item, id) {
+  document.getElementById(id).innerHTML = item;
+}
+
+function startStop() {
+  if (started == false) {
+    countdown = setInterval(function() {
+      time++;
+      display(time, 'stopwatch');
+    }, 100)
+    started = true;
+  } else {
+    clearInterval(countdown);
+    started = false;
+  }
+}
+
+function reset() {
+  time = 0;
+  display(time, 'stopwatch');
+  display('', 'pasttimes');
+}
+
+function recTime() {
+  document.getElementById('pasttimes').innerHTML += time + "<br>";
+}
+
+function init() {
+  display(0, 'stopwatch')
+
+  // Obtenenos variables del DOM
+  var startStopButton = document.getElementById('startstop');
+  var resetButton = document.getElementById('reset');
+  var recTimeButton = document.getElementById('rectime');
+  
+  // Eventos
+  startStopButton.addEventListener('click', function() {
+    startStop();
+  });
+  
+  resetButton.addEventListener('click', function() {
+    reset();
+  });
+  
+  recTimeButton.addEventListener('click', function() {
+    recTime();
+  });
+  
+  // Eventos desde teclado
+  document.addEventListener('keydown', function() {
+    if (event.key == "s") {
+      startStop();
+    } else if (event.key == "t") {
+      reset();
+    } else if (event.key == 'r') {
+      recTime();
+    } else {
+      console.log('Invalid Key: ' + event.key)
+    }
+  });
+}
